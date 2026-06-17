1787c0c03acc8d91f9b79b04c2a22c4845af2c65
NICOLLY RODRIGUES DE OLIVEIRA <nicolly_oliveira151@estudante.sesisenai.org.br>
2026-06-17 16:00:31 -0300

    atividade 4

--- exercicio01.js
+++ exercicio01.js
@@ -4,10 +4,34 @@
 
 //exercicio 2
 
-let nome = "joao silva";
-let cargo = "Desenvolvedor Front-End"
-const experiencia = "2 anos"
+// let nome = "joao silva";
+// let cargo = "Desenvolvedor Front-End"
+// const experiencia = "2 anos"
 
-console.log(nome)
-console.log(cargo)
-console.log(experiencia)
+// console.log(nome)
+// console.log(cargo)
+// console.log(experiencia)
+
+// exercicio 3
+
+//Crie uma variável contendo o valor total vendido. Calcule uma bonificação 
+// correspondente  a 10% desse valor
+//  e exiba o valor vendido, o valor da bonificação e o valor total.
+
+// let totalvendido = 5000;
+// let bonificacao = totalvendido * 0.10;
+// let valorotal = totalvendido + bonificacao;
+
+// console.log(" vendas : " + totalvendido)
+// console.log("bonificacao : " + bonificacao)
+// console.log("total : " + valorotal)
+
+// exercicio 4
+let idade =  18;
+ if(idade >= 18){
+    console.log("autorizado")
+ } else if(idade< 18){
+    console.log("negado")
+ } else {
+    console.log("negado")
+ }
