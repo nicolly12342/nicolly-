71c48fa00597a0f6515dbfcff4d9e940ccf194ae
NICOLLY RODRIGUES DE OLIVEIRA <nicolly_oliveira151@estudante.sesisenai.org.br>
2026-06-17 14:48:23 -0300

    atividade 2

--- exercicio01.js
+++ exercicio01.js
@@ -1,1 +1,13 @@
-console.log("Bem-vindo ao ambiente de desenvolvimento JavaScript!");
+// exercicio 1
+
+// console.log("Bem-vindo ao ambiente de desenvolvimento JavaScript!");
+
+//exercicio 2
+
+let nome = "joao silva";
+let cargo = "Desenvolvedor Front-End"
+const experiencia = "2 anos"
+
+console.log(nome)
+console.log(cargo)
+console.log(experiencia)
